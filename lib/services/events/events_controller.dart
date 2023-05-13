b5026fc37d86d2321e597f6dcbeaa34a6a8517bc
JarellOng <jarellong20@gmail.com>
2023-05-13 22:01:14 +0700

    Add Create Event Implementation

--- lib/services/events/events_controller.dart
+++ lib/services/events/events_controller.dart
@@ -15,21 +15,22 @@ class EventController {
   EventController._sharedInstance();
   factory EventController() => _shared;
 
-  // TODO: Add Appropriate Start and End Datatypes
   Future<void> create({
-    required String categoryId,
     required String title,
-    required String description,
-    required bool allDay,
+    required String categoryId,
+    required Timestamp start,
+    required Timestamp end,
     required bool important,
+    String? description,
   }) async {
     final events = await _getCollection();
     await events.add({
-      eventCategoryIdField: categoryId,
       eventTitleField: title,
-      eventDescriptionField: description,
-      eventAllDayField: allDay,
-      eventImportantField: important
+      eventCategoryIdField: categoryId,
+      eventStartField: start,
+      eventEndField: end,
+      eventImportantField: important,
+      eventDescriptionField: description
     });
   }
 
