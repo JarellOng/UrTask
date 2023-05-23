74780f44fc6800cae9fe4f1c9daf7541deb4a8be
JarellOng <jarellong20@gmail.com>
2023-05-23 21:16:49 +0700

    Rebase Main

--- lib/views/calendar_view.dart
+++ lib/views/calendar_view.dart
@@ -23,8 +23,7 @@ class CalendarView extends StatefulWidget {
 
 class _CalendarViewState extends State<CalendarView> {
   DateTime selectedDay = DateTime.now();
-   CalendarFormat calendar = CalendarFormat.month; 
-  DateTime pilihanDay = DateTime.now();
+  CalendarFormat calendar = CalendarFormat.month;
   late final CalendarController _calendarService;
 
   @override
@@ -43,80 +42,83 @@ class _CalendarViewState extends State<CalendarView> {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(child: Column(
-      children: [
-        Container(
-          padding: const EdgeInsets.only(
-            top: 20.0,
-          ),
-          child: TableCalendar(
-            focusedDay:
-                _calendarService.showToday(today: widget.today) ?? selectedDay,
-            firstDay: DateTime.utc(2010, 10, 16),
-            lastDay: DateTime.utc(2025, 10, 16),
-            rowHeight: 45,
-            calendarFormat: widget.calendarFilter,
-            headerVisible: false,
-            onDaySelected: (DateTime selectDay, DateTime focusDay) {
-              setState(() {
-                selectedDay = selectDay;
-              });
-              widget.selectedDate.text =
-                  selectedDay.toString().substring(0, 10);
-            },
-            onPageChanged: (focusedDay) {
-              setState(() {
-                selectedDay = focusedDay;
-              });
-              widget.today.text = DateFormat('yMMMM').format(selectedDay);
-              widget.selectedDate.text =
-                  selectedDay.toString().substring(0, 10);
-            },
-            eventLoader: (day) {
-              return [day];
-            },
-            selectedDayPredicate: (DateTime date) {
-              return isSameDay(selectedDay, date);
-            },
-            calendarStyle: const CalendarStyle(
-              weekendTextStyle: TextStyle(color: Colors.red),
-              selectedDecoration: BoxDecoration(
-                shape: BoxShape.circle,
-                color: primary,
-              ),
-              todayDecoration: BoxDecoration(
-                shape: BoxShape.circle,
-                color: tertiary,
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Container(
+            padding: const EdgeInsets.only(
+              top: 20.0,
+            ),
+            child: TableCalendar(
+              focusedDay: _calendarService.showToday(today: widget.today) ??
+                  selectedDay,
+              firstDay: DateTime.utc(2010, 10, 16),
+              lastDay: DateTime.utc(2025, 10, 16),
+              rowHeight: 45,
+              calendarFormat: widget.calendarFilter,
+              headerVisible: false,
+              onDaySelected: (DateTime selectDay, DateTime focusDay) {
+                setState(() {
+                  selectedDay = selectDay;
+                });
+                widget.selectedDate.text =
+                    selectedDay.toString().substring(0, 10);
+              },
+              onPageChanged: (focusedDay) {
+                setState(() {
+                  selectedDay = focusedDay;
+                });
+                widget.today.text = DateFormat('yMMMM').format(selectedDay);
+                widget.selectedDate.text =
+                    selectedDay.toString().substring(0, 10);
+              },
+              eventLoader: (day) {
+                return [day];
+              },
+              selectedDayPredicate: (DateTime date) {
+                return isSameDay(selectedDay, date);
+              },
+              calendarStyle: const CalendarStyle(
+                weekendTextStyle: TextStyle(color: Colors.red),
+                selectedDecoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: primary,
+                ),
+                todayDecoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: tertiary,
+                ),
               ),
             ),
           ),
-        ),
-        const Divider(
-          height: 2,
-          thickness: 2,
-          color: Colors.black26,
-        ),
-        Padding(
-          padding: const EdgeInsets.only(top: 12.0, bottom: 16.0, left: 16.0),
-          child: Align(
-              alignment: Alignment.topLeft,
-              child: Text(DateFormat('yMMMMd').format(selectedDay),
-                  style: const TextStyle(
-                      fontSize: 25, fontWeight: FontWeight.w600))),
-        ),
-        
-        if(widget.calendarFilter == calendar)...[
-          ConstrainedBox(constraints: const BoxConstraints(
-            maxHeight: 330.0,
-              ), child: EventView(selectedDay: selectedDay))
-        ] else...[
-          ConstrainedBox(constraints: const BoxConstraints(
-            maxHeight: 510.0,
-              ), child: EventView(selectedDay: selectedDay))
-        ]
-        
-        
-      ],
+          const Divider(
+            height: 2,
+            thickness: 2,
+            color: Colors.black26,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 12.0, bottom: 16.0, left: 16.0),
+            child: Align(
+                alignment: Alignment.topLeft,
+                child: Text(DateFormat('yMMMMd').format(selectedDay),
+                    style: const TextStyle(
+                        fontSize: 25, fontWeight: FontWeight.w600))),
+          ),
+          if (widget.calendarFilter == calendar) ...[
+            ConstrainedBox(
+                constraints: const BoxConstraints(
+                  maxHeight: 330.0,
+                ),
+                child: EventView(selectedDay: selectedDay))
+          ] else ...[
+            ConstrainedBox(
+                constraints: const BoxConstraints(
+                  maxHeight: 510.0,
+                ),
+                child: EventView(selectedDay: selectedDay))
+          ]
+        ],
+      ),
     );
   }
 }
